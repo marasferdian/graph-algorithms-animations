c17a3d281dd11af798d1fd9a53cd3b46306f23d8
Mara Sferdian <marasferdian@gmail.com>
2020-10-16 13:25:35 +0300

    Deleted unnecessary files

--- main.py
+++ main.py
@@ -15,20 +15,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    n = 10
-    adj_list = random_graph(n, 12)
-    labels = 'ABCDEFGHIJ'
-    labelsDict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10}
-
-    locations = find_optimal_coords(n, adj_list, verbose=True, tolerance=1e-4, max_iter=1000, mutation_rate=0.4,
-                                    curved_edges=False, spring_mode='edges_only', node_spacing_factor=0.5)
-    highlighted_1 = (255, 0, 0)
-    highlighted_2 = (0, 0, 255)
-    unhighlighted = (127, 127, 127)
-    print(adj_list)
-    anim = GraphAnimation(n, adj_list, locations[:, 0], locations[:, 1], labels=labels, initial_color=unhighlighted)
-    anim.next_frame()
-    anim.next_frame()
-    rand = np.random.randint(100000000)
-    # anim.save_gif('dijkstra%d.gif' % rand, node_radius=20, size=(800, 800), fps=1.2)
-    # anim.save_json('dijkstra%d.json' % rand)
+    print('main')
